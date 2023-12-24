ecb0ccc1f938ffa5e7e4a3a2d0fc535276052526
Juan Carlos Moreno Sanchez <yeiden@Juans-MacBook-Air.local>
2023-12-24 12:54:23 -0600

    Adding the function to get the convert action

--- app/main.py
+++ app/main.py
@@ -1,7 +1,32 @@
+import pandas as pd
+from enum import Enum
 
-def doRun ():
-    print("Hello world!")
+
+class Convert:
+    def __init__(self, name: str, action) -> None:
+        self.name = name
+        self.action = action
+
+
+class ActionToFill(Enum):
+    AVG = 1
+    NONE_FILL = 2
+
+def find(name: str, actions) -> Convert :
+    return list(filter(lambda item: item.name == name, actions))
+
+def doRun(name_file: str, actions):
+    csv_data = pd.read_csv(name_file)
+    for column in csv_data.columns:
+        filtered = find(name=column, actions=actions)
+        if len(filtered) == 1:
+            print(filtered[0].name)
 
 
 if __name__ == "__main__":
-    doRun()
+    actions = []
+    actions.append(
+        Convert(name='AREA_HARVESTED_BED_PLOT_M2:(m2)', action=ActionToFill.AVG))
+    actions.append(
+        Convert(name='AREA_SOWN_BED_PLOT_M2:(m2)', action=ActionToFill.AVG))
+    doRun(name_file='/Users/yeiden/repo/test_py/origin_test.csv', actions=actions)
