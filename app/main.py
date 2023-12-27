b505a28fdf8bdfe81feab9d79b8583b0c51e2168
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2023-12-26 19:53:58 -0600

    Create a new file and convert nan on avg

--- app/main.py
+++ app/main.py
@@ -12,21 +12,32 @@ class ActionToFill(Enum):
     AVG = 1
     NONE_FILL = 2
 
-def find(name: str, actions) -> Convert :
+
+def find(name: str, actions) -> Convert:
     return list(filter(lambda item: item.name == name, actions))
 
-def doRun(name_file: str, actions):
+
+def doRun(save_file: str, name_file: str, actions):
     csv_data = pd.read_csv(name_file)
     for column in csv_data.columns:
         filtered = find(name=column, actions=actions)
         if len(filtered) == 1:
             print(filtered[0].name)
-            data = csv_data.loc[:,column]
-            print(data)
-            data2 = list(filter(lambda i: not pd.isnull(i), data))
-            print(data2)
-            print(sum(filter(lambda i: isinstance(i, float), data)))
+            elements = list(
+                filter(lambda i: not pd.isnull(i), csv_data.loc[:, column]))
+            csv_data[column] = csv_data[column].fillna(
+                sum(elements) / len(elements))
             # TODO:  remplace all null with avg
+    save(pd=csv_data, name=save_file)
+
+
+def save(pd, name: str, index:bool = False) -> None:
+    from os import path, remove
+    if path.exists(name):
+        remove(name)
+    pd.to_csv(name, index=index)
+
+    pd.to_csv()
 
 
 if __name__ == "__main__":
@@ -35,4 +46,8 @@ if __name__ == "__main__":
         Convert(name='AREA_HARVESTED_BED_PLOT_M2:(m2)', action=ActionToFill.AVG))
     actions.append(
         Convert(name='AREA_SOWN_BED_PLOT_M2:(m2)', action=ActionToFill.AVG))
-    doRun(name_file='/Users/yeiden/repo/test_py/origin_test.csv', actions=actions)
+    doRun(
+        save_file='test.csv',
+        name_file='./origin_test.csv',
+        actions=actions
+    )
