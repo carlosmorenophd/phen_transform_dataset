0ad319702f6795070db5aef7d932c959d5777242
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2023-12-26 21:56:24 -0600

    Create a new rules to normal and prepar to create a rule for dates

--- app/main.py
+++ app/main.py
@@ -12,6 +12,8 @@ class ActionToFill(Enum):
     AVG = 1
     FILL_CERO = 2
     ENUM_NONE = 3
+    ENUM_NORMAL = 4
+
 
 
 def find(name: str, actions) -> Convert:
@@ -24,15 +26,21 @@ def doRun(save_file: str, name_file: str, actions):
         filtered = find(name=column, actions=actions)
         if len(filtered) == 1:
             if filtered[0].action == ActionToFill.AVG:
-            print(filtered[0].name)
-            elements = list(
-                filter(lambda i: not pd.isnull(i), csv_data.loc[:, column]))
-            csv_data[column] = csv_data[column].fillna(
-                sum(elements) / len(elements))
+                elements = list(
+                    filter(lambda i: not pd.isnull(i), csv_data.loc[:, column]))
+                csv_data[column] = csv_data[column].fillna(
+                    sum(elements) / len(elements))
+            elif filtered[0].action == ActionToFill.ENUM_NONE:
+                csv_data[column] = csv_data[column].fillna('NONE')
+            elif filtered[0].action == ActionToFill.FILL_CERO:
+                csv_data[column] = csv_data[column].fillna(0)
+            elif filtered[0].action == ActionToFill.ENUM_NORMAL:
+                csv_data[column] = csv_data[column].fillna('NORMAL')
+            # TODO: create a new function to put a date according the avg between the snow date
     save(pd=csv_data, name=save_file)
 
 
-def save(pd, name: str, index:bool = False) -> None:
+def save(pd, name: str, index: bool = False) -> None:
     from os import path, remove
     if path.exists(name):
         remove(name)
@@ -46,8 +54,11 @@ if __name__ == "__main__":
     actions.append(
         Convert(name='AREA_SOWN_BED_PLOT_M2:(m2)', action=ActionToFill.AVG))
     actions.append(
-        Convert(name='BIRD_DAMAGE:(N/T/S/M/V)', action=ActionToFill.AVG))
+        Convert(name='BIRD_DAMAGE:(N/T/S/M/V)', action=ActionToFill.ENUM_NONE))
+    actions.append(
+        Convert(name='EMERGENCE:(E/N/L)', action=ActionToFill.ENUM_NORMAL))
     
+
     doRun(
         save_file='test.csv',
         name_file='./origin_test.csv',
