9198a1e2bc41569cf18e3a7075977b767d291f66
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2024-01-07 11:56:58 -0600

    Fix problems with GPS Adding new rule to validate GPS Adding filter by every experiment

--- app/weather/main.py
+++ app/weather/main.py
@@ -3,8 +3,6 @@ import pandas as pd
 import requests_cache
 
 
-
-
 class FormatEnum(Enum):
     JSON = "json"
 
@@ -17,13 +15,46 @@ class OperationEnum(Enum):
     AVG_ALL = 1
 
 
+class ParameterEnum(Enum):
+    ALLSKY_SFC_SW_DWN = "ALLSKY_SFC_SW_DWN"
+    CLRSKY_SFC_SW_DWN = "CLRSKY_SFC_SW_DWN"
+    ALLSKY_KT = "ALLSKY_KT"
+    ALLSKY_SFC_LW_DWN = "ALLSKY_SFC_LW_DWN"
+    ALLSKY_SFC_PAR_TOT = "ALLSKY_SFC_PAR_TOT"
+    CLRSKY_SFC_PAR_TOT = "CLRSKY_SFC_PAR_TOT"
+    ALLSKY_SFC_UVA = "ALLSKY_SFC_UVA"
+    ALLSKY_SFC_UVB = "ALLSKY_SFC_UVB"
+    ALLSKY_SFC_UV_INDEX = "ALLSKY_SFC_UV_INDEX"
+    T2M = "T2M"
+    T2MDEW = "T2MDEW"
+    T2MWET = "T2MWET"
+    TS = "TS"
+    T2M_RANGE = "T2M_RANGE"
+    T2M_MAX = "T2M_MAX"
+    T2M_MIN = "T2M_MIN"
+    QV2M = "QV2M"
+    RH2M = "RH2M"
+    PRECTOTCORR = "PRECTOTCORR"
+    PS = "PS"
+    WS10M = "WS10M"
+    WS10M_MAX = "WS10M_MAX"
+    WS10M_MIN = "WS10M_MIN"
+    WS10M_RANGE = "WS10M_RANGE"
+    WD10M = "WD10M"
+    WS50M = "WS50M"
+    WS50M_MAX = "WS50M_MAX"
+    WS50M_MIN = "WS50M_MIN"
+    WS50M_RANGE = "WS50M_RANGE"
+    WD50M = "WD50M"
+
+
 def do_run(
     name_file: str,
     latitude_column: str,
     longitude_column: str,
     start_date_column: str,
     end_date_column: str,
-    parameters: list,
+    parameters: list(ParameterEnum),
     save_file: str,
     format: FormatEnum = FormatEnum.JSON,
     community: CommunityEnum = CommunityEnum.RE,
@@ -36,24 +67,24 @@ def do_run(
     csv["date_end"] = pd.to_datetime(csv[end_date_column])
     csv["date_end"] = csv["date_end"].dt.strftime('%Y%m%d')
     session = requests_cache.CachedSession('wheat_cache')
-    for param in parameters:
+    for paramEnum in parameters:
+        param = paramEnum.value
         csv[param] = 0.0
-    for index, row in csv.iterrows():
-        params = {
-            "start": row["date_start"],
-            "end": row["date_end"],
-            "latitude": row[latitude_column],
-            "longitude": row[longitude_column],
-            "community": community.value,
-            "parameters": ','.join(parameters),
-            "format": format.value,
-            "user": "cloud",
-            "header": True,
-            "time-standard": 'lst',
-        }
-        response = session.get(url=url, params=params)
-        result = response.json()["properties"]["parameter"]
-        for param in parameters:
+        for index, row in csv.iterrows():
+            params = {
+                "start": row["date_start"],
+                "end": row["date_end"],
+                "latitude": row[latitude_column],
+                "longitude": row[longitude_column],
+                "community": community.value,
+                "parameters": param,
+                "format": format.value,
+                "user": "cloud",
+                "header": True,
+                "time-standard": 'lst',
+            }
+            response = session.get(url=url, params=params)
+            result = response.json()["properties"]["parameter"]
             if operation == OperationEnum.AVG_ALL:
                 avg = sum(result[param].values()) / float(len(result[param]))
                 csv.at[index, param] = avg
@@ -66,7 +97,8 @@ if __name__ == "__main__":
     longitude_column = "GPS Longitude (Decimal)"
     start_date_column = "SOWING_DATE:(date)"
     end_date_column = "HARVEST_STARTING_DATE:(date)"
-    parameters = ["ALLSKY_SFC_SW_DWN"]
+    parameters = [ParameterEnum.ALLSKY_SFC_SW_DWN,
+                  ParameterEnum.CLRSKY_SFC_SW_DWN]
     save_file = "test_wheat.csv"
     do_run(
         name_file=name_file,
