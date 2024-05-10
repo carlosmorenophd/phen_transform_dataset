36a7be6cc6669759ef69e887ff66d2a1ef46340c
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2024-05-10 10:01:31 -0500

    GEt all

--- app/main.py
+++ app/main.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
        column_date="Date"
     )
     weather.transform_date(
-        start='2015-12-13',
-        end='2016-02-16',
-        group_by=GroupByDateEnum.SEVEN_DAY,
+        start='2016-12-13',
+        end='2017-02-16',
+        group_by=GroupByDateEnum.FIFTEEN_DAY,
         operation_group= OperationToGroupEnum.AVG,
         is_debug= True,
     )
-    weather.save_transform(file_path="data/sevent_day_obregon_2015.csv")
+    weather.save_transform(file_path="data/fifteen_day_obregon_2016.csv")
