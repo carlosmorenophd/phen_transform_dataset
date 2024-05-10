b688f2c7e27f10897ffff76583581af639e4b840
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2024-05-10 09:27:25 -0500

    Fixing the data to get all new data

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from weather.csv.csv_extracted import WeatherSourceCSV
-from weather.csv.enum import GroupByDateEnum
+from weather.csv.enum import GroupByDateEnum, OperationToGroupEnum
 from pandas import to_datetime
 import os
 
@@ -7,9 +7,14 @@ import os
 if __name__ == "__main__":
     print(os.getcwd())
     weather = WeatherSourceCSV(
-        file_path="data/obregon/obregon_weather.csv", column_date="Date")
-    pd = weather.get_all_by_date(
-        start=to_datetime('2015-12-1', format='%Y-%m-%d'),
-        end=to_datetime('2015-12-2', format='%Y-%m-%d'),
-        group_by=GroupByDateEnum.BIG_SINGLE_AVG,
+        file_path="data/obregon/weather_151617_obregon.csv", 
+       column_date="Date"
     )
+    weather.transform_date(
+        start='2015-12-13',
+        end='2016-02-16',
+        group_by=GroupByDateEnum.SEVEN_DAY,
+        operation_group= OperationToGroupEnum.AVG,
+        is_debug= True,
+    )
+    weather.save_transform(file_path="data/sevent_day_obregon_2015.csv")
