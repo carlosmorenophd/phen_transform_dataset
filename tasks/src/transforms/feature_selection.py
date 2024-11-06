344f65d5264b9c5608225eb576484e45cb6e5278
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2024-11-06 14:11:49 -0600

    Adding new rule to feature selection

--- tasks/src/transforms/feature_selection.py
+++ tasks/src/transforms/feature_selection.py
@@ -1,8 +1,9 @@
 """Select variables by criteria"""
+import re
 from src.support.utils import get_file_to_data_frame, FolderCache, save_to_csv
 
 
-def select_column_by_percentage_values(file_in: str, percentage: float) -> None:
+def select_column_by_text_like(file_in: str, percentage: float) -> None:
     """Keep only columns that are more or equal to percentage
     Args:
         file_in: File to get the data and is in upload file folder
@@ -12,8 +13,37 @@ def select_column_by_percentage_values(file_in: str, percentage: float) -> None:
         raise ValueError("Percentage only be 0 to 1")
     df = get_file_to_data_frame(file_name=file_in, folder=FolderCache.UPLOAD)
     percentage_no_null = df.notnull().mean()
-    select_columns = percentage_no_null[percentage_no_null > percentage].index.values
+    select_columns = percentage_no_null[percentage_no_null >
+                                        percentage].index.values
     print(select_columns)
     df_filter = df[select_columns]
     file_out = f"select_percentage_{file_in}"
+    save_to_csv(data_frame=df_filter, file_save=file_out)
+
+
+def select_column_by_patter_like(file_in: str, patter_like: str) -> None:
+    """Keep only columns that have same str of patters
+    Args:
+        file_in: File to get the data and is in upload file folder
+        patter_like: str of patterns
+    """
+    df = get_file_to_data_frame(file_name=file_in, folder=FolderCache.UPLOAD)
+    patter = re.compile(patter_like, re.IGNORECASE)
+    df_filter = df.filter(regex=patter)
+    print(df_filter.columns.values)
+    file_out = f"select_patter_{patter_like}_{file_in}"
+    save_to_csv(data_frame=df_filter, file_save=file_out)
+
+
+def select_column_by_list_patter_and(file_in: str, patters_like: list) -> None:
+    """Keep only columns that have same list of patters
+    Args:
+        file_in: File to get the data and is in upload file folder
+        patter_like: List of patterns
+    """
+    df = get_file_to_data_frame(file_name=file_in, folder=FolderCache.UPLOAD)
+    patter = re.compile(r"*".join(patters_like), re.IGNORECASE)
+    df_filter = df.filter(regex=patter)
+    print(df_filter.columns.values)
+    file_out = f"select_patter_{patters_like}_{file_in}"
     save_to_csv(data_frame=df_filter, file_save=file_out)
--- tasks/test_task.py
+++ tasks/test_task.py
@@ -2,6 +2,8 @@
 
 
 import sys
+import re
+import pandas as pd
 
 from celery.app import Celery
 from src.support.utils import REDIS_BROKEN
@@ -14,6 +16,20 @@ def keep_percentage_of_no_empty(file_csv, percentage) -> None:
                   args=(file_csv, percentage))
 
 
+def keep_patter_like(file_csv: str, patter_like: str) -> None:
+    """Send task to remove keep only column like patter"""
+    app = Celery('phen_transform', broker_url=REDIS_BROKEN)
+    app.send_task(name="feature_selection-keep_patter_like",
+                  args=(file_csv, patter_like))
+
+
+def keep_patter_list_like(file_csv: str, patters_like: str) -> None:
+    """Send task to remove keep only column like patters list with and"""
+    app = Celery('phen_transform', broker_url=REDIS_BROKEN)
+    app.send_task(name="feature_selection-keep_patter_list_like",
+                  args=(file_csv, patters_like))
+
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         print(f"Run with props action - {sys.argv[1]}")
@@ -24,11 +40,17 @@ if __name__ == "__main__":
             print("Run - keep_percentage_of_no_empty")
             print(f"file - {sys.argv[2]}, percentage - {sys.argv[3]}")
             keep_percentage_of_no_empty(file_csv=param1, percentage=param2)
-
+        elif action == "keep_patter_like":
+            print("Run - keep_patter_like")
+            print(f"file - {sys.argv[2]}, percentage - {sys.argv[3]}")
+            keep_patter_like(file_csv=param1, patter_like=param2)
+        elif action == "keep_patter_list_like":
+            print("Run - keep_patter_list_like")
+            print(f"file - {sys.argv[2]}, percentage - {sys.argv[3]}")
+            keep_patter_list_like(file_csv=param1, patters_like=param2)
         else:
             print("Not action valid")
 
     else:
-        print("Run no case")
-
+        print("Only test")
     print("end")
